fe59c65bcfb5d45facf2fd48d6e9e02e4c080142
Kalabasa <Kalabasa@users.noreply.github.com>
2023-10-06 02:40:35 +1100

    testing

--- src/pages/japan-2023/content.ts
+++ src/pages/japan-2023/content.ts
@@ -1,5 +1,7 @@
-import { arrangeGalleryPhotos } from "ui/article/gallery_helper";
+import { arrangeGalleryPhotos } from "ui/article/arrange_gallery_photos";
 import photo1 from "./photos/photo.jpg";
+import port from "./photos/port.png";
+import land from "./photos/land.png";
 
 export default createFakeContent();
 
@@ -8,9 +10,9 @@ function createFakeContent() {
     "Spring in Japan! This collection of images offers a glimpse into the country's captivating landscapes, vibrant cities, and cultural richness.";
 
   const photos = arrangeGalleryPhotos(
-    Array.from({ length: 12 }, () => ({
+    Array.from({ length: 12 }, (_, i) => ({
       alt: "hello",
-      imageMetadata: photo1,
+      imageMetadata: [photo1, port, land][i % 3],
       size: "SML"[Math.floor(Math.random() * 3)] as any,
     }))
   );
--- src/ui/article/arrange_gallery_photos.ts
+++ src/ui/article/arrange_gallery_photos.ts
@@ -1,9 +1,25 @@
 import { Props as PhotoProps } from "ui/photo/photo.astro";
 
+const colWidth = 205;
+const rowHeight = 40;
+
 const spanMap = {
-  S: [4, 3],
-  M: [4, 4],
-  L: [6, 4],
+  S: [
+    [1, 2],
+    [1, 3],
+    [1, 4],
+  ],
+  M: [
+    [1, 4],
+    [1, 6],
+    [2, 3],
+  ],
+  L: [
+    [1, 6],
+    [1, 8],
+    [2, 4],
+    [2, 6],
+  ],
 };
 
 type PhotoConfig = {
@@ -17,9 +33,22 @@ export function arrangeGalleryPhotos(photos: PhotoConfig[]): PhotoProps[] {
     const compiledPhoto: Partial<PhotoProps> = {};
     compiledPhoto.alt = photo.alt;
     compiledPhoto.imageMetadata = photo.imageMetadata;
-    const [colSpan, rowSpan] = spanMap[photo.size];
-    compiledPhoto.colSpan = colSpan;
-    compiledPhoto.rowSpan = rowSpan;
+
+    const imageRatio = photo.imageMetadata.width / photo.imageMetadata.height;
+    const spans = spanMap[photo.size];
+    let bestSpan = spans[0];
+    let bestScore = Infinity;
+    for (const span of spans) {
+      const sizeRatio = (span[0] * colWidth) / (span[1] * rowHeight);
+      const score = Math.abs(Math.log(sizeRatio) - Math.log(imageRatio));
+      if (bestScore > score) {
+        bestSpan = span;
+        bestScore = score;
+      }
+    }
+    compiledPhoto.colSpan = bestSpan[0];
+    compiledPhoto.rowSpan = bestSpan[1];
+
     return compiledPhoto as PhotoProps;
   });
 }
